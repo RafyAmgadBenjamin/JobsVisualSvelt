20dfe7d3591c942f72165ec39801cc20e65a4139
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-08-21 13:07:14 +0200

    making the navigation bar as a separte component and refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,17 @@
 	import Router from 'svelte-spa-router';
 	import Workers from './routes/Workers.svelte';
 	import Tasks from './routes/Tasks.svelte';
+	import Navigtation from './Navigation.svelte';
 	// import active from 'svelte-spa-router';
-	import routes from './routes'
-	import {link, push, pop, replace, location, querystring} from 'svelte-spa-router'
+	import routes from './routes';
+	import {
+		link,
+		push,
+		pop,
+		replace,
+		location,
+		querystring,
+	} from 'svelte-spa-router';
 </script>
 
 <style>
@@ -13,18 +21,7 @@
 
 <!--Navigtation bar-->
 <div>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Jobs visualization bannel </a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="/#/workers">Workers</a></li>
-      <li><a href="/#/tasks">Tasks</a></li>
-    </ul>
-  </div>
-</nav>
+	<Navigtation />
 </div>
 <h1>parent</h1>
 <div class="container">
